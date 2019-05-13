5b5e90a2cf639e43de5dcd9dffd0c1aadc9e65ee
Jaisel Rahman <jaiselrahman@gmail.com>
2019-05-13 22:38:39 +0530

    Refactor to androidx

--- chatbot/src/main/java/com/flaminus2k18/flaminuschatbot/ChatBotActivity.java
+++ chatbot/src/main/java/com/flaminus2k18/flaminuschatbot/ChatBotActivity.java
@@ -2,10 +2,10 @@ package com.flaminus2k18.flaminuschatbot;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
